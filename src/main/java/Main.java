551972f1c8717e61350c6a622f8f2200e8eff05c
Administrator <806516984@qq.com>
2021-05-11 20:54:39 +0800

    uuu

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,11 @@
+import com.hero.DiaoChan;
+
 import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-
+        DiaoChan<String> diaoChan = new DiaoChan<>();
+        String integer = "";
+        diaoChan.printClass(integer);
     }
 }
